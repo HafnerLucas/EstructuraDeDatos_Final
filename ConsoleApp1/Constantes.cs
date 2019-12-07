bf2b1cad74c97b8a9c0d7c33ddc4e46c48e12c81
Haffy <lucas.ezequiel.hafner@outlook.com>
2019-12-07 16:01:40 -0300

    version final

--- ConsoleApp1/Constantes.cs
+++ ConsoleApp1/Constantes.cs
@@ -6,7 +6,7 @@ namespace Lucas
 {
     static class Constantes
     {
-        public const string Formato_Patente_Anterior2016 = "[(A-Z)(a-z)]{3}[0-9]{3}";
-        public const string Formato_Patente_Actual = "[(A-Z)(a-z)]{2}[0-9]{3}[(A-Z)(a-z)]{2}";
+        public const string Formato_Patente_Anterior2016 = "[(A-Z)(a-z)]{3}[0-9]{3}$";
+        public const string Formato_Patente_Actual = "[(A-Z)(a-z)]{2}[0-9]{3}[(A-Z)(a-z)]{2}$";
     }
 }
